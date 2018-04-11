b11031ee5a0e03d59f95afb15b11fd6c6d355b9e
Bharanitharan.k <bharanitharan1117@live.com>
2018-04-11 12:57:16 +0530

    chat app socket.io

---
+++ app.js
@@ -0,0 +1,74 @@
+var express=require('express');
+var app=express();
+var http=require('http');
+var ejs = require('ejs');
+app.use(express.static('web'));
+var server=http.createServer(app);
+var io=require('socket.io')(server);
+var name="";
+app.set('views',__dirname+'/views');
+app.set('view engine', 'ejs');
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/web/index.html');
+})
+app.post('/chat',function(req,res){
+    res.render('chat',{username:name});
+    console.log(name);
+})
+app.get('/chat',function(req,res){
+    res.redirect('/');
+})
+server.listen(8080);
+var con=[{id:'0',uname:'$'}];
+io.on('connection',function(socket){
+    socket.on('check',function(username){
+        val=check(username)
+        if(val===-1)
+        {
+            name=username;
+            socket.emit('success');
+        }
+        else
+        {
+            socket.emit('failure');
+        }
+    })
+    socket.on('chat',function(data){
+        connect(socket.id,data.user);
+        console.log(socket.id,data.user);
+        socket.broadcast.emit('message',data);
+        socket.emit('message',data);
+    })
+    socket.on('disconnect', function() {
+        for(x in con){
+        if(con[x].id==socket.id){
+           socket.broadcast.emit('unplug',{data:con[x].name});
+           console.log('disconnet'+socket.id);
+           delete con[x];
+        }
+       }
+    })
+})
+function connect(id,uname){
+    key=0;
+    for(x in con){
+       if(con[x].name===uname){
+           con[x].id=id;
+           key=1;
+       }
+    }
+    if(key===0)
+    {
+        con.push({id:id,name:uname});
+        console.log(con);
+    }
+}
+function check(username){
+    key=-1;
+    for(x in con){
+        if(con[x].name===username){
+           key=1;
+       }
+    }
+    return key;
+}
